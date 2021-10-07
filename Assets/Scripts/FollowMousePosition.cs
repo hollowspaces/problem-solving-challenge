926a31fabb07a65e051604a0ab01a7d1dcea30a0
Puji <hollowspacesz@gmail.com>
2021-10-07 22:41:41 +0700

    solving problem 5

---
+++ Assets/Scripts/FollowMousePosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMousePosition : MonoBehaviour
+{
+    private float Speed = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, mousePosition, Speed * Time.deltaTime);
+    }
+}
