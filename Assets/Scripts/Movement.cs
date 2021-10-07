72397e6533bd2e61fbcc25734dcbf04422893ddf
Puji <hollowspacesz@gmail.com>
2021-10-07 20:09:42 +0700

    solving problem 4

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float Speed = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Translate(input * Speed * Time.deltaTime);
+    }
+}
