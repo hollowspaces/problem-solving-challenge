d1c0f10b759d4746fb7c7913a1e02498586f79a9
Puji <hollowspacesz@gmail.com>
2021-10-08 13:59:02 +0700

    solving problem 6

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Circle;
+    public GameObject Outline;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMousePosition followMousePosition = Circle.AddComponent<FollowMousePosition>();
+        followMousePosition.MousePosition();
+    }
+}
