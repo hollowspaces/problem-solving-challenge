98cf28912035e07588b8300d14d43422c87c4bd7
Puji <hollowspacesz@gmail.com>
2021-10-09 21:59:01 +0700

    solving problem 9

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private List<AudioClip> _audioClips;
+
+    private static AudioManager _instance = null;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public void PlaySfx (string name)
+    {
+        AudioClip sfx = _audioClips.Find(s => s.name == name);
+        if (sfx == null)
+        {
+            return;
+        }
+        _audioSource.PlayOneShot(sfx);
+    }
+}
---
+++ Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBox : MonoBehaviour
+{
+    public GameObject Box;
+
+    public int minSpawn = 15; 
+    public int maxSpawn = 40;
+    public int totalBox;
+
+    private Vector2 _randomPos;
+    private int _randomSpawn;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _randomSpawn = Random.Range(minSpawn, maxSpawn);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {   
+        if (totalBox < _randomSpawn)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        bool boxSpawned = false;
+        while (!boxSpawned)
+        {
+            Vector3 boxPos = new Vector3(Random.Range(-7.6f, 7.6f), Random.Range(-3.8f, 3.8f), 0f);
+            if ((boxPos - transform.position).magnitude < 2)
+            {
+                continue;
+            } else
+            {
+                Instantiate(Box, boxPos, Quaternion.identity);
+                totalBox += 1;
+                boxSpawned = true;
+            }
+        }
+    }
+}
